43713cb3f82f2efdfa91869a5e8433b8e84776ca
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2026-06-19 12:30:56 -0700

    Update movement to accelerate towards zero

--- main.gd
+++ main.gd
@@ -4,5 +4,6 @@ func _ready():
     pass
 
 func _process(_delta):
-    if Input.is_action_just_pressed("quit"):
+    if Input.is_action_just_pressed("quit") and OS.has_feature("pc"):
+        print("exit requested")
         get_tree().quit()
--- player.gd
+++ player.gd
@@ -1,13 +1,12 @@
 extends CharacterBody3D
 
-@export var acceleration_direction: Direction
+@export var acceleration = 0.5
+@export var acceleration_direction =  Direction.EAST
 
 enum Direction {
     NORTH,
     EAST
 }
-
-const ACCELERATION = 0.5
 
 const MOVEMENT_ACTIONS = ["movement_0", "movement_1", "movement_2", "movement_3"]
 
@@ -28,30 +27,37 @@ func _physics_process(delta):
 
         print("Changing acceleration_direction: ", self.acceleration_direction)
 
-    var speed_change = 0
+    var speed_change = null
     for i in MOVEMENT_ACTIONS.size():
         if Input.is_action_just_pressed(MOVEMENT_ACTIONS[i]):
             # Only make any changes for adjacent keys being pressed (modulo 4)
-            # TODO: should we "cancel" movement if the keys are pressed out-of-order?
-            # e.g. accelerate towards zero, or something like that
-
             if last_action_index == (i + 1) % MOVEMENT_ACTIONS.size():
                 speed_change = -1
             elif last_action_index == (i - 1) % MOVEMENT_ACTIONS.size():
                 speed_change = 1
+            else:
+                # Accelerate towards v=0
+                speed_change = 0
 
             last_action_index = i
 
-    match acceleration_direction:
-        Direction.NORTH:
-            if speed_change != 0:
-                velocity.z += ACCELERATION * speed_change
-            direction_indicator.rotation.y = 0
-            direction_indicator.scale.z = -velocity.z
-        Direction.EAST:
-            if speed_change != 0:
-                velocity.x += ACCELERATION * speed_change
-            direction_indicator.rotation.y = PI / 2.0
-            direction_indicator.scale.z = -velocity.x
+    if speed_change != null:
+        match acceleration_direction:
+            Direction.NORTH:
+                if speed_change != 0:
+                    velocity.z += acceleration * -speed_change
+                else:
+                    velocity.z += acceleration * -sign(velocity.z)
+
+                direction_indicator.rotation.y = 0
+                direction_indicator.scale.z = -velocity.z
+            Direction.EAST:
+                if speed_change != 0:
+                    velocity.x += acceleration * speed_change
+                else:
+                    velocity.x += acceleration * -sign(velocity.x)
+
+                direction_indicator.rotation.y = PI / 2.0
+                direction_indicator.scale.z = -velocity.x
 
     move_and_slide()
