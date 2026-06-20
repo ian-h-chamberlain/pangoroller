d7d5e676194160ab72464336616c45379227f06a
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2026-06-20 13:11:53 -0700

    Add a world boundary + automatic reset

--- main.gd
+++ main.gd
@@ -1,9 +1,17 @@
 extends Node
 
-func _ready():
-    pass
+@export var player: Player
+
+@onready var initial_player_transform: Transform3D = $Player.transform
 
 func _process(_delta):
     if Input.is_action_just_pressed("quit") and OS.has_feature("pc"):
         print("exit requested")
         get_tree().quit()
+
+
+func _on_world_boundary_body_exited(body: Node3D):
+    if body == player:
+        # Reset the player to start
+        $Player.transform = initial_player_transform
+        $Player.velocity = Vector3.ZERO
